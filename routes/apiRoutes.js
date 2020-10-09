656daac2c608024b184e9306eebbc7cd94e3e681
Maxx Sanner <maxxsanner105@gmail.com>
2020-10-08 20:49:20 -0700

    added mongoose schema and added routes to apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,25 @@
 const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+        .then(dbWorkout => {
+            console.log(dbWorkout)
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
 module.exports = router;
