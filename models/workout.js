427682c6a7363d6f3ee3cc611f830e14b15c5e87
Maxx Sanner <maxxsanner105@gmail.com>
2020-10-10 18:28:02 -0700

    added post and put routes

--- models/workout.js
+++ models/workout.js
@@ -4,44 +4,44 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
 
-  day: {
+    day: {
     type: Date,
     default: Date.now
-  },
-  exercises: [
-    {
-        type: {
-            type: String,
-            trim: true,
-            required: "Enter type of workout"
-        },
-        name: {
-            type: String,
-            trim: true,
-            required: "Enter name of workout"
-        },
-        duration: {
-            type: Number,
-            validate: [({ length }) => length >= 0, "Duration should be at least 0."] 
-        },
-        weight: {
-            type: Number,
-            validate: [({ length }) => length >= 0, "Weight should be at least 0."] 
-        },
-        reps: {
-            type: Number,
-            validate: [({ length }) => length >= 0, "Reps should be at least 0."] 
-        },
-        sets: {
-            type: Number,
-            validate: [({ length }) => length >= 0, "Sets should be at least 0."] 
-        },
-        distance: {
-            type: Number,
-            validate: [({ length }) => length >= 0, "Distance should be at least 0."] 
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter type of workout"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter name of workout"
+            },
+            duration: {
+                type: Number,
+                validate: [({ length }) => length >= 0, "Duration should be at least 0."] 
+            },
+            weight: {
+                type: Number,
+                validate: [({ length }) => length >= 0, "Weight should be at least 0."] 
+            },
+            reps: {
+                type: Number,
+                validate: [({ length }) => length >= 0, "Reps should be at least 0."] 
+            },
+            sets: {
+                type: Number,
+                validate: [({ length }) => length >= 0, "Sets should be at least 0."] 
+            },
+            distance: {
+                type: Number,
+                validate: [({ length }) => length >= 0, "Distance should be at least 0."] 
+            }
         }
-    }
-  ]
+    ]
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,6 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.post("/api/workouts", (req, res) => {
-    Workout.create({})
-        .then(dbWorkout => {
-            console.log(dbWorkout)
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-});
-
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
         .then(dbWorkout => {
@@ -21,5 +10,38 @@ router.get("/api/workouts", (req, res) => {
             res.status(400).json(err);
         });
 });
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create(req)
+        .then(dbWorkout => {
+            console.log(dbWorkout);
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, {exercises: req.body}, {new: true})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });  
+});
+
+
 
 module.exports = router;
